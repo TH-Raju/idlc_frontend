e6e1ca3f284bc6938cc7e8ba1618a2fa55873e0a
TH-Raju <rajukhan894200@gmail.com>
2024-05-06 12:15:41 +0600

    frontend structure complete

--- src/Components/Shared/SubmitButton.jsx
+++ src/Components/Shared/SubmitButton.jsx
@@ -10,7 +10,7 @@ const SubmitButton = ({ title }) => {
       className="w-full border-none font-bold uppercase"
       style={{ backgroundColor: "#0A6847", color: "white" }}
     >
-      {title} {/* Access title as a string */}
+      {title}
     </Button>
   );
 };
